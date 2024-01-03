a3a5db1579af7bcabda74651ebd3a244a14d7501
SHUBHAM GUPTA <shubham.gup1811@gmail.com>
2024-01-03 13:54:57 +0530

    changes

--- custom_addons/dotpl_helpdesk/models/__init__.py
+++ custom_addons/dotpl_helpdesk/models/__init__.py
@@ -3,6 +3,6 @@ from . import helpdesk_stage
 from . import helpdesk_tag
 from . import helpdesk_team
 from . import helpdesk_team_member
-from . import helpdesk_ticket
 from . import helpdesk_ticket_category
 from . import helpdesk_ticket_source
+from . import helpdesk_ticket
