a030900995b4225f7055d9c700c7ec1515d05ad4
SHUBHAM GUPTA <shubham.gup1811@gmail.com>
2024-01-03 13:07:26 +0530

    sla policy changes

--- custom_addons/dotpl_helpdesk/models/helpdesk_sla_policy.py
+++ custom_addons/dotpl_helpdesk/models/helpdesk_sla_policy.py
@@ -7,7 +7,10 @@ TICKET_PRIORITY = [('0', 'Very Low'), ('1', 'Low'), ('2', 'Moderate'), ('3', 'Hi
 class HelpdeskSlaPolicy(models.Model):
     _name = "helpdesk.sla.policy"
     _description = "Helpdesk SLA Policies"
+    _order = 'id desc'
 
     name = fields.Char(string='Name', required=True)
     active = fields.Boolean(string='Active', default=True)
-    priority = fields.Selection(TICKET_PRIORITY, string='Priority')
+    priority = fields.Selection(TICKET_PRIORITY, string='Priority', required=True)
+    time = fields.Float(string='Within', default=0, required=True,
+                        help='Maximum number of working hours a ticket should take to reach the target stage, starting from the date it was created.')
--- custom_addons/dotpl_helpdesk/models/helpdesk_ticket.py
+++ custom_addons/dotpl_helpdesk/models/helpdesk_ticket.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from odoo import api, fields, models, _
 
 TICKET_PRIORITY = [('0', 'Very Low'), ('1', 'Low'), ('2', 'Moderate'), ('3', 'High'), ('4', 'Very High')]
@@ -17,7 +19,7 @@ class HelpdeskTicket(models.Model):
     activity = fields.Char(string='Activity',default='Remove activity')
     stage_id = fields.Many2one(comodel_name='helpdesk.stage', string='Stage', default=_default_stage_id, tracking=1)
 
-    created_date = fields.Date(string='Created Date', default=fields.Date.context_today)
+    created_datetime = fields.Datetime(string='Created Date', default=fields.Datetime.now, tracking=True)
     ticket_number = fields.Char(string='Ticket Number', readonly=True)
 
     category_id = fields.Many2one(comodel_name='helpdesk.ticket.category', string='Category', tracking=True)
@@ -31,7 +33,7 @@ class HelpdeskTicket(models.Model):
     member_id = fields.Many2one(comodel_name='res.users', string='Assigned To', domain="[('id', 'in', possible_team_member_ids)]", tracking=True)
     priority = fields.Selection(selection=TICKET_PRIORITY, string='Priority', default='1', tracking=True)
     tag_ids = fields.Many2many(comodel_name='helpdesk.tag', string='Tags')
-    estimated_closing_date = fields.Date(string='Estimated Closing Date')
+    sla_deadline = fields.Datetime(string='SLA Deadline')
     closing_date = fields.Date(string='Closing Date')
 
     customer_name = fields.Char(string='Customer Name')
@@ -40,9 +42,19 @@ class HelpdeskTicket(models.Model):
     cc_email = fields.Char(string='cc Email')
     uploaded_document = fields.Binary(string='Document', attachment=True)
 
+    @api.onchange('created_datetime', 'priority')
+    def onchange_created_datetime_priority(self):
+        sla_policies = self.env['helpdesk.sla.policy'].search([('priority', '=', self.priority)],
+                                                              order='id desc', limit=1)
+        if sla_policies:
+            duration = timedelta(hours=sla_policies[0].time)
+            self.sla_deadline = self.created_datetime + duration
+
+
     # ------------------------------------------------------------
     # CRUD
     # ------------------------------------------------------------
+
 
     @api.model
     def create(self, vals):
