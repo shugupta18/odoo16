dcb7137729a72077db6b86976f14445ece02ee36
SHUBHAM GUPTA <shubham.gup1811@gmail.com>
2024-01-09 14:42:07 +0530

    Adding "Vehicles" smart button in "Hub" and "Branch"

--- custom_addons/dotpl_fleet/models/fleet_branch.py
+++ custom_addons/dotpl_fleet/models/fleet_branch.py
@@ -17,17 +17,43 @@ class FleetBranch(models.Model):
     fleet_manager = fields.Char(string='Fleet Manager Name', tracking=True)
     manager_email = fields.Char(string='Manager Email', tracking=True)
     fleet_manager_email = fields.Char(string='Fleet Manager Email', tracking=True)
+    branch_email = fields.Char(string='Branch Email')
 
-    hub_ids = fields.One2many(comodel_name='fleet.hub', inverse_name='branch_id', string='Hubs')
     hub_count = fields.Integer(string='', compute='_compute_hub_count', store=True)
+    vehicle_count = fields.Integer(compute='_compute_vehicle_count', store=True)
+    hub_ids = fields.One2many(comodel_name='fleet.hub', inverse_name='branch_id', string='Hubs')
 
     @api.depends('hub_ids')
     def _compute_hub_count(self):
         for branch in self:
             branch.hub_count = len(branch.hub_ids)
 
-    def action_view_hubs(self):
+    def _compute_vehicle_count(self):
+        for branch in self:
+            branch.vehicle_count = self.env['fleet.vehicle'].search_count([
+                ('branch_id', '=', branch.id),
+            ])
+
+    # ------------------------------------------------------------
+    # ACTIONS
+    # ------------------------------------------------------------
+
+    def action_model_hub(self):
         action = self.env.ref('dotpl_fleet.action_fleet_hub').read()[0]
         action['domain'] = [('branch_id', '=', self.id)]
         action['context'] = {'default_branch_id': self.id}
         return action
+
+    def action_model_vehicle(self):
+        self.ensure_one()
+        view = {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'kanban,tree,form',
+            'res_model': 'fleet.vehicle',
+            'name': _('Vehicles'),
+            'context': {
+                'search_default_branch_id': self.id,
+                'default_branch_id': self.id,
+            }
+        }
+        return view
--- custom_addons/dotpl_fleet/models/fleet_hub.py
+++ custom_addons/dotpl_fleet/models/fleet_hub.py
@@ -10,17 +10,35 @@ class FleetHub(models.Model):
     address = fields.Char(string='Address')
     lat = fields.Char(string='Latitude')
     long = fields.Char(string='Longitude')
-
     erp_code = fields.Char(string='Hub ERP Code', tracking=True)
-
     manager = fields.Char(string='Manager', tracking=True)
     fleet_manager = fields.Char(string='Fleet Manager', tracking=True)
     manager_email = fields.Char(string='Manager Email', tracking=True)
     fleet_manager_email = fields.Char(string='Fleet Manager Email', tracking=True)
-
     email = fields.Char(string='Hub Email')
     operations_email = fields.Char(string='Hub Operations Email')
     fleet_email = fields.Char(string='Hub Fleet Email')
 
+    vehicle_count = fields.Integer(compute='_compute_vehicle_count', store=True)
     branch_id = fields.Many2one('fleet.branch', string="Branch")
 
+    def _compute_vehicle_count(self):
+        for hub in self:
+            hub.vehicle_count = self.env['fleet.vehicle'].search_count([
+                ('hub_id', '=', hub.id),
+            ])
+
+    def action_model_vehicle(self):
+        self.ensure_one()
+        view = {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'kanban,tree,form',
+            'res_model': 'fleet.vehicle',
+            'name': _('Vehicles'),
+            'context': {
+                'search_default_hub_id': self.id,
+                'default_branch_id': self.branch_id.id,
+                'default_hub_id': self.id,
+            }
+        }
+        return view
