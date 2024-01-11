935b02d3309ac4e0940f9e3ccbe289172f8af633
SHUBHAM GUPTA <shubham.gup1811@gmail.com>
2024-01-11 13:33:54 +0530

    changes

--- custom_addons/dotpl_fleet/models/fleet_vehicle_inheritance.py
+++ custom_addons/dotpl_fleet/models/fleet_vehicle_inheritance.py
@@ -6,7 +6,8 @@ class FleetVehicleInheritance(models.Model):
 
     branch_id = fields.Many2one('fleet.branch', string='Branch')
     hub_id = fields.Many2one('fleet.hub', string='Hub', domain="[('branch_id', '=', branch_id)]")
-    document_ids = fields.One2many('fleet.document', 'vehicle_id', string='Documents', domain=[('state', '=', 'active')])
+    document_ids = fields.One2many('fleet.document', 'vehicle_id', string='Documents', domain=[('state', 'in', ('active', 'expired', 'hold'))])
+    history_document_ids = fields.One2many('fleet.document', 'vehicle_id', string='History Documents', domain=[('state', '=', 'renewed')])
 
 
 
