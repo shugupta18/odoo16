d517a323c207a1cfbcb22f0766fe045f1b42a0e7
SHUBHAM GUPTA <shubham.gup1811@gmail.com>
2024-01-11 14:50:46 +0530

    changes

--- custom_addons/dotpl_fleet/models/fleet_document_type.py
+++ custom_addons/dotpl_fleet/models/fleet_document_type.py
@@ -11,7 +11,7 @@ class DocumentType(models.Model):
     active = fields.Boolean(string='Active', default=True, tracking=True)
     expire_in_days = fields.Integer(string='Expire in (Days)', default=0, tracking=True)
     reminder_required_in_days = fields.Integer(string='Reminder Required in (Days)', default=0, tracking=True)
-    recurring_flag = fields.Boolean(string='Recurring/Checklist', default=True, help='True means its a "Recurring" document')
+    recurring_flag = fields.Boolean(string='Is Recurring?', default=True, help='True means its a "Recurring" document')
 
     category_id = fields.Many2one('fleet.document.type.category', string='Document Category', required=True)
 
